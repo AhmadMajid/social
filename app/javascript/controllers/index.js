f4585341fc40d9522292ec8132b271647f899c8d
Ahmad Majid <Ahmadmajid1994@gmail.com>
2023-04-09 10:59:14 +0100

    Submit vibrations with Turbo

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,3 +6,6 @@ import { application } from "./application"
 
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
+
+import VibrationFormController from "./vibration_form_controller"
+application.register("vibration-form", VibrationFormController)
