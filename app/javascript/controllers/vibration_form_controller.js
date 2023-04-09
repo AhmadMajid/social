ab18047342d3685cfe652c36135a908fa6f27df7
Ahmad Majid <Ahmadmajid1994@gmail.com>
2023-04-09 11:43:42 +0100

    Set user's display names

--- app/javascript/controllers/vibration_form_controller.js
+++ app/javascript/controllers/vibration_form_controller.js
@@ -1,6 +1,6 @@
 import { Controller } from "@hotwired/stimulus"
 
-// Connects to data-controller="tweet-form"
+// Connects to data-controller="vibration-form"
 export default class extends Controller {
   connect() {
     this.element.addEventListener('turbo:submit-end', () => {
