8735db8e876bc505bd37bcb4346ee88b1cf97d0e
Ahmad Majid <Ahmadmajid1994@gmail.com>
2023-04-16 10:08:53 +0100

    Rendering vibrations on users profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,8 @@ class UsersController < ApplicationController
   def show
     redirect_to profile_path if params[:id].to_i == current_user.id
     @user = User.find(params[:id])
+    @vibration_presenters = @user.vibrations.map do |vibration|
+      VibrationPresenter.new(vibration: vibration, current_user: @user)
+    end
   end
 end
