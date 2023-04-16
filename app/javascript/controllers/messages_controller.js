3552a49fcb9d565624377c78d7076f231353b34d
Ahmad Majid <Ahmadmajid1994@gmail.com>
2023-04-16 21:37:12 +0100

    Submitting messages and rendering it

--- app/javascript/controllers/messages_controller.js
+++ app/javascript/controllers/messages_controller.js
@@ -13,7 +13,7 @@ export default class extends Controller {
 
         fetch(`/messages/${messageThread.dataset.messageThreadId}/messages`, { headers: this.headers })
           .then(response => response.text())
-          .then(html => Turbo.renderStreamMessage(html));
+          .then(html => Turbo.renderStreamMessage(html))
       });
     });
   }
