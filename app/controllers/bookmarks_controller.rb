1ec9d6eb78ffeee8ecc51d49eb66213d14c60c08
Ahmad Majid <Ahmadmajid1994@gmail.com>
2023-04-16 10:24:41 +0100

    Rendering bookmarks

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,6 +1,12 @@
 class BookmarksController < ApplicationController
   before_action :authenticate_user!
 
+  def index
+    @vibration_presenters = current_user.bookmarked_vibrations.map do |vibration|
+      VibrationPresenter.new(vibration: vibration, current_user: current_user)
+    end
+  end
+
   def create
     @bookmark = current_user.bookmarks.create(vibration: vibration)
     respond_to do |format|
